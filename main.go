6f6c30c49dca188b3115be7f5f9d25bc89100c01
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-10-07 11:50:02 -0500

    error handling
    saw some spots where errors could be more apparent

--- main.go
+++ main.go
@@ -12,27 +12,27 @@ import (
 )
 
 func main() {
-	// Create a new KeyValueStore
+	// create a new key-value store
 	store := libkeva.NewKeyValueStore("data.json", 5*time.Second)
 
-	// Load initial data from file if it exists
-	err := store.LoadFromFile("data.json")
-	if err != nil {
-		log.Fatal(err)
+	// load initial data from file if it exists
+	if err := store.LoadFromFile("data.json"); err != nil {
+		log.Fatalf("Failed to load data from file: %v", err)
 	}
 
+	// start a goroutine to print statistics
 	go printStats(store)
 
-	// Create HTTP server with CORS middleware
+	// create http server with cors middleware
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		switch r.Method {
 		case http.MethodGet:
-			// Check if path starts with "/x/"
+			// check if path starts with "/x/"
 			if len(r.URL.Path) > 3 && r.URL.Path[:3] == "/x/" {
 				key := r.URL.Path[3:]
-				// Insert or update row with key
+				// insert or update row with key
 				value, exists := store.Get(key)
 				if !exists {
 					value = 0
@@ -47,29 +47,31 @@ func main() {
 		}
 	})
 
-	// Start HTTP server
-	log.Fatal(http.ListenAndServe(":3589", handler))
+	// start http server
+	if err := http.ListenAndServe(":3589", handler); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
 
 func printStats(store *libkeva.KeyValueStore) {
-	// Start goroutine that prints statistics every 1 second
+	// start goroutine that prints statistics every 1 second
 	for {
 		data := store.GetData()
 		fmt.Print("\033[2J\n" + time.Now().String()[:19] + "\n")
 		fmt.Printf("Total keys: %d\n\n", len(data))
 
-		// Create tabwriter with padding of 4 spaces
+		// create tabwriter with padding of 4 spaces
 		writer := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
 
-		// Print header row
+		// print header row
 		fmt.Fprintln(writer, "Key\tHits")
 
-		// Print rows for all keys
+		// print rows for all keys
 		for key, value := range data {
 			fmt.Fprintf(writer, "%s\t%d\n", key, value)
 		}
 
-		// Flush tabwriter buffer
+		// flush tabwriter buffer
 		writer.Flush()
 
 		time.Sleep(1 * time.Second)
